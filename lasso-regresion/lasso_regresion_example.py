f64edbd54aba3d557966a14b302bc1b782260609
Yorhagy <32210821+Yorhagy@users.noreply.github.com>
2018-10-18 17:12:10 -0500

    Lasso Ready

--- lasso-regresion/lasso_regresion_example.py
+++ lasso-regresion/lasso_regresion_example.py
@@ -26,58 +26,47 @@ standardize_const = (Xtrain_mean, Xtrain_var, Ytrain_mean, Ytrain_var)
 
 #Calcular regresion lasso
 
-tetha = lasso.regresion(Xtrain_std, Ytrain_std, 1)
+tetha, success, n, costs = lasso.regression(Xtrain_std, Ytrain_std, 10)
 
-
-#Predecir las salidas usando los modelos
-#pred_gradient = rigid.predict(tetha_gradient, Xtrain, standardize_const)
-#pred_direct = rigid.predict(tetha_direct, Xtrain, standardize_const)
-
-#Calcular el error para los dos metodos
-#error_gradient = rigid.error(pred_gradient, Ytrain)
-#error_direct = rigid.error(pred_direct, Ytrain)
-#print('\nError Gradient: ', error_gradient)
-#print('Error Direct: ', error_direct)
-
-#Graficar la dinamica del costo de gradiente descendiente
-#plt.plot(np.arange(0, n,1),costs)
-#plt.xlabel('n')
-#plt.ylabel('Cost')
-#plt.title('Gradient Descent Cost History')
+#Graficar la dinamica del costo de Lasso
+plt.plot(np.arange(0, n,1),costs)
+plt.xlabel('n')
+plt.ylabel('Cost')
+plt.title('Lasso Cost History')
 
 #Calcular multiples regresiones variando d2
-# delta2 = np.arange(0, 200, 1)
-# tetha = np.zeros((len(delta2), X.shape[1], Y.shape[1]))
-# error_train = np.zeros(len(delta2))
-# error_test = np.zeros(len(delta2))
-# for i in range(len(delta2)):
-#   #Calcular regresion usando funcion directa
-#   tetha[i,:,:] = rigid.direct(Xtrain_std, Ytrain_std, delta2[i])
-#   #Calcular errores de entrenamiento y de validacion
-#   pred_train = rigid.predict(tetha[i,:,:], Xtrain, standardize_const)
-#   pred_test = rigid.predict(tetha[i,:,:], Xtest, standardize_const)
-#   error_train[i] = rigid.error(pred_train, Ytrain)
-#   error_test[i] = rigid.error(pred_test, Ytest)
+delta2 = np.arange(0, 60, 1)
+tethas = np.zeros((len(delta2), X.shape[1], Y.shape[1]))
+error_train = np.zeros(len(delta2))
+error_test = np.zeros(len(delta2))
+for i in range(len(delta2)):
+    #Calcular regresion usando funcion directa
+    t = lasso.regression(Xtrain_std, Ytrain_std, delta2[i])[0]
+    tethas[i,:,:] = t
+    #Calcular errores de entrenamiento y de validacion
+    pred_train = lasso.predict(tethas[i,:,:], Xtrain, standardize_const)
+    pred_test = lasso.predict(tethas[i,:,:], Xtest, standardize_const)
+    error_train[i] = lasso.error(pred_train, Ytrain)
+    error_test[i] = lasso.error(pred_test, Ytest)
    
-# #Graficar la variacion de los parametros de la regresion al variar d2
-# plt.figure()
-# for i in range(X.shape[1]):
-#   for j in range(Y.shape[1]):
-#     plt.plot(delta2, tetha[:,i,j], label='(%d,%d)'%(i,j))
-#     plt.hold
-# plt.xlabel('delta2')
-# plt.ylabel('Lineal Parameters')
-# plt.title('Regularization')
-# plt.legend()
+#Graficar la variacion de los parametros de la regresion al variar d2
+plt.figure()
+for i in range(X.shape[1]):
+    for j in range(Y.shape[1]):
+        plt.plot(delta2, tethas[:,i,j], label='(%d,%d)'%(i,j))
+        plt.hold
+plt.xlabel('delta2')
+plt.ylabel('Lineal Parameters')
+plt.title('Regularization')
+plt.legend()
 
-# #Graficar la variacion del error al variar d2
-# plt.figure()
-# plt.plot(delta2, error_train, label='Training Error')
-# plt.hold
-# plt.plot(delta2, error_test, label='Testing Error')
-# plt.xlabel('delta2')
-# plt.ylabel('Medium Relative Cuadratic Error')
-# plt.title('Prediction Error')
-# plt.legend()
-
-# plt.show()
+#Graficar la variacion del error al variar d2
+plt.figure()
+plt.plot(delta2, error_train, label='Training Error')
+plt.hold
+plt.plot(delta2, error_test, label='Testing Error')
+plt.xlabel('delta2')
+plt.ylabel('Medium Relative Cuadratic Error')
+plt.title('Prediction Error')
+plt.legend()
+plt.show()
